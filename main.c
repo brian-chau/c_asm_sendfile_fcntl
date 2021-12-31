cb0831cf433ce960a92d546f5c327ce3723f6e0d
Brian Chau <brian.chau@outlook.com>
2021-12-31 12:52:49 -0800

    Updated main.S to use the correct constant for F_SETFL, removed superfluous stuff from main.c

--- main.c
+++ main.c
@@ -5,11 +5,7 @@
 
 int main(int argc, char** argv){
     int old_flags = fcntl(STDOUT_FILENO, F_GETFD);
-    printf("%d\n", old_flags);
     fcntl(STDOUT_FILENO, F_SETFL, old_flags & ~O_APPEND);
     sendfile(STDOUT_FILENO, open("a.txt", O_RDONLY), 0, 1000);
-    printf("\n");
-    printf("%d\n", F_GETFD);
-    printf("%d\n", F_SETFD);
     return 0;
 }
